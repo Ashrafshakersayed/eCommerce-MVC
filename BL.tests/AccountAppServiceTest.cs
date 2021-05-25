6af0fdfce88caa21cb21dd18bf4e1fd5d9197809
Ashraf Shaker <sayedashrafshaker@yahoo.com>
2021-05-25 10:53:07 +0200

    Add project files.

---
+++ BL.tests/AccountAppServiceTest.cs
@@ -0,0 +1,23 @@
+﻿using BL.AppServices;
+using DAL;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BL.tests
+{
+    class AccountAppServiceTest
+    {
+        AccountAppService accountAppService = new AccountAppService();
+        [Test]
+        public void FindTest()
+        {
+            var result = accountAppService.Find("ss", "123456");
+
+            Assert.That(result, Is.EqualTo(new ApplicationUserIDentity()));
+        }
+    }
+}
